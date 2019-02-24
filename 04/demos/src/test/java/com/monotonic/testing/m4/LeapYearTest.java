67727b10632ed821bb270e45910df4ceac558f1f
Zach Lee <zach.lee@concur.com>
2019-02-23 23:23:45 -0800

    Add tests and code

--- 04/demos/src/test/java/com/monotonic/testing/m4/LeapYearTest.java
+++ 04/demos/src/test/java/com/monotonic/testing/m4/LeapYearTest.java
@@ -10,7 +10,24 @@ public class LeapYearTest {
     // a year is a leap year if it is divisible by four
     // but, years divisible by 100, are not leap years,
     // except years divisible by 400
+    @Test
+    public void leapYearsAreDivisibleByFour() {
+        assertTrue(LeapYear.isLeapYear(2016));
+    }
 
+    @Test
+    public void normalYearsAreNotDivisibleByFour() {
+        assertFalse(LeapYear.isLeapYear(3));
+    }
+
+    @Test
+    public void leapYearsAreNotDivisibleByOneHundred() {
+        assertFalse(LeapYear.isLeapYear(1900));
+    }
+
+    @Test
+    public void leapYearsAreDivisibleByFourHundred() {
+        assertTrue(LeapYear.isLeapYear(2000));
     }
 
 }

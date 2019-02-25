43a66c33d1eff6c9868a5b4ed73ac5fe5d95a27d
Zach Lee <zach.lee@concur.com>
2019-02-24 21:50:51 -0800

    Extract variable

--- 04/demos/src/main/java/com/monotonic/testing/m4/WordWrap.java
+++ 04/demos/src/main/java/com/monotonic/testing/m4/WordWrap.java
@@ -5,12 +5,13 @@ import static java.lang.Math.min;
 public class WordWrap {
     public static String wrap(final String inputLine, final int lineLength) {
         final StringBuilder accumulator = new StringBuilder();
+        final int length = inputLine.length();
 
-        accumulator.append(inputLine, 0, min(inputLine.length(), lineLength));
+        accumulator.append(inputLine, 0, min(length, lineLength));
 
-        if (inputLine.length() > lineLength) {
+        if (length > lineLength) {
             accumulator.append('\n');
-            accumulator.append(inputLine, lineLength, inputLine.length());
+            accumulator.append(inputLine, lineLength, length);
         }
 
         return accumulator.toString();

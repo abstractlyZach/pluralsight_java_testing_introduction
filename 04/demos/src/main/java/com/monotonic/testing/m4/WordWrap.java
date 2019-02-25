9c19cb6bed64aaea32bba72543b16178f3253db1
Zach Lee <zach.lee@concur.com>
2019-02-24 21:42:07 -0800

    Change variables to final

--- 04/demos/src/main/java/com/monotonic/testing/m4/WordWrap.java
+++ 04/demos/src/main/java/com/monotonic/testing/m4/WordWrap.java
@@ -3,13 +3,13 @@ package com.monotonic.testing.m4;
 import static java.lang.Math.min;
 
 public class WordWrap {
-    public static String wrap(String inputLine, int lineLength) {
-        StringBuilder sb = new StringBuilder();
+    public static String wrap(final String inputLine, final int lineLength) {
+        final StringBuilder accumulator = new StringBuilder();
 
-        sb.append(inputLine, 0, lineLength);
-        sb.append('\n');
-        sb.append(inputLine, lineLength, inputLine.length());
+        accumulator.append(inputLine, 0, lineLength);
+        accumulator.append('\n');
+        accumulator.append(inputLine, lineLength, inputLine.length());
 
-        return sb.toString();
+        return accumulator.toString();
     }
 }
